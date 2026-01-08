44060a6363ea2d635efbfe7724d4a0e93033da6b
Arun Bansode <arunbansode00@gmail.com>
2026-01-08 21:32:52 +0530

    Initial commit

---
+++ src/main/java/com/example/myproject1/Hello.java
@@ -0,0 +1,12 @@
+package com.example.myproject1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/")
+        public String index(){
+        return "Hello MIT";
+        }
+}
